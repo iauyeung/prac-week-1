4a591f0e0b31a0e34d9962858b251af572c0d39a
iauyeung <87755675+iauyeung@users.noreply.github.com>
2021-09-15 17:35:00 +1000

    Add files via upload

---
+++ practical_1.js
@@ -0,0 +1,11 @@
+var vue_practical_1 = new Vue({
+    el: '#vue_practical_1',
+    data: {
+        todos: [
+            { text: 'shrimp for Gura' },
+            { text: 'shrimp for Haachama' },
+            { text: 'shrimp for Mel' },
+            { text: 'shrimp for Korone' }
+        ]
+    }
+})
